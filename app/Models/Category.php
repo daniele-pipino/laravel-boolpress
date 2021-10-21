cbab7934632d4bea406eb4766fc32de9ddcb0bab
daniele-pipino <Danielemacpipino@icloud.com>
2021-10-21 16:55:15 +0200

    models relation

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    // collegamento al modello post
+    public function posts()
+    {
+        return $this->hasMany('App\Models\Post');
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    // correlazione modello 
+    public function category()
+    {
+        return $this->belongsTo('App\Models\Category');
+    }
 }
