095e6bbc6d8edcbe43326ff4382d2a8b7589217b
daniele-pipino <Danielemacpipino@icloud.com>
2021-10-22 16:54:24 +0200

    add post user relation 1 to many

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,4 +14,9 @@ class Post extends Model
     {
         return $this->belongsTo('App\Models\Category');
     }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
